079d478ff9545aef5690ebb496b30b080aa2a103
Mani <maniataai@gmail.com>
2025-01-11 15:43:50 +0330

    faq add not responsive

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,11 +1,109 @@
 import React from 'react'
 
 function Questions() {
-  return (
-    <div className='mt-8'>
-        
-    </div>
-  )
+    return (
+        <div className='mt-10 flex items-center flex-row-reverse justify-between w-full'>
+
+            <div className='scale-[1.40]' >
+                <img src="/nav-icon/faq.png"  className='' alt="" />
+            </div>
+
+            <div className='flex flex-col gap-4'>
+                <div className='mb-6 text-2xl font-xbold'>
+                    سوالات متداول
+                </div>
+
+                <div className="collapse collapse-arrow bg-base-200 w-[850px] bg-white">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium bg-white border-b ">
+                        <div className='flex gap-8'>
+                            
+                            <div>
+                                <img src="/nav-icon/soal.png" alt="" />
+                            </div>
+
+                            <div>
+                                چه مدارکی برای ثبت‌نام در تورهای داخلی نیاز است؟
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="collapse-content bg-white ">
+                        <p>برای ثبت‌نام در تورهای داخلی، شما نیاز به کارت شناسایی معتبر (کارت ملی یا شناسنامه) .</p>
+                    </div>
+                </div>
+
+                <div className="collapse collapse-arrow bg-base-200 w-[850px] bg-white">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium bg-white border-b ">
+                        <div className='flex gap-8'>
+                            
+                            <div>
+                                <img src="/nav-icon/soal.png" alt="" />
+                            </div>
+
+                            <div>
+                                چه مدارکی برای ثبت‌نام در تورهای داخلی نیاز است؟
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="collapse-content bg-white ">
+                        <p>برای ثبت‌نام در تورهای داخلی، شما نیاز به کارت شناسایی معتبر (کارت ملی یا شناسنامه) .</p>
+                    </div>
+                </div>
+
+                <div className="collapse collapse-arrow bg-base-200 w-[850px] bg-white">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium bg-white border-b ">
+                        <div className='flex gap-8'>
+                            
+                            <div>
+                                <img src="/nav-icon/soal.png" alt="" />
+                            </div>
+
+                            <div>
+                                چه مدارکی برای ثبت‌نام در تورهای داخلی نیاز است؟
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="collapse-content bg-white ">
+                        <p>برای ثبت‌نام در تورهای داخلی، شما نیاز به کارت شناسایی معتبر (کارت ملی یا شناسنامه) .</p>
+                    </div>
+                </div>
+
+                <div className="collapse collapse-arrow bg-base-200 w-[850px] bg-white">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium bg-white border-b ">
+                        <div className='flex gap-8'>
+                            
+                            <div>
+                                <img src="/nav-icon/soal.png" alt="" />
+                            </div>
+
+                            <div>
+                                چه مدارکی برای ثبت‌نام در تورهای داخلی نیاز است؟
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="collapse-content bg-white ">
+                        <p>برای ثبت‌نام در تورهای داخلی، شما نیاز به کارت شناسایی معتبر (کارت ملی یا شناسنامه) .</p>
+                    </div>
+                </div>
+
+                
+               
+
+
+            </div>
+        </div>
+    )
 }
 
 export default Questions
+
+
+
+
